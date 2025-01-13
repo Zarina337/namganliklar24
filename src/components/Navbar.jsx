b9bb36d7a9cf7a144f611bb253e58cb038e97442
Zarina Ahmadjanova <Zarina05092011@gmail.com>
2025-01-13 19:55:47 +0500

    navbar logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router"
+
+function Navbar() {
+    return (
+        <>
+            <div className="my-Navbar pt-4 pb-4">
+                <div className="container">
+                    <div>
+                        <div>
+
+<Link to={'/'} className="navbar-logo">NAMAGANLIKALAR24</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Navbar
